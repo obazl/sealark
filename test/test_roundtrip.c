fda8ac6c07dddb83741f8e76c8457391dfbdbea9
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2021-07-03 13:36:44 -0500

    add tests to test:roundtrip

--- test/test_roundtrip.c
+++ test/test_roundtrip.c
@@ -24,6 +24,13 @@ void tearDown(void) {
     utstring_free(buffer);
 }
 
+void test_android(void) {
+    TEST_ASSERT_MESSAGE(
+        roundtrip("test/data/BUILD.android") == EXIT_SUCCESS,
+        "Mismatch on test/data/BUILD.android"
+    );
+}
+
 void test_cpp(void) {
     TEST_ASSERT_MESSAGE(
         roundtrip("test/data/BUILD.cpp") == EXIT_SUCCESS,
@@ -31,21 +38,31 @@ void test_cpp(void) {
     );
 }
 
-void test_gen(void) {
+void test_general_rules(void) {
+    TEST_ASSERT_MESSAGE(
+        roundtrip("test/data/BUILD.filegroup") == EXIT_SUCCESS,
+        "Mismatch on test/data/BUILD.filegroup"
+    );
+
     TEST_ASSERT_MESSAGE(
         roundtrip("test/data/BUILD.gen") == EXIT_SUCCESS,
         "Mismatch on test/data/BUILD.gen"
     );
 }
 
-void test_filegroup(void) {
+void test_load(void) {
     TEST_ASSERT_MESSAGE(
-        roundtrip("test/data/BUILD.filegroup") == EXIT_SUCCESS,
-        "Mismatch on test/data/BUILD.filegroup"
+        roundtrip("test/data/BUILD.load_1") == EXIT_SUCCESS,
+        "Mismatch on test/data/BUILD.load_1"
     );
-}
-
-void test_load_aliases(void) {
+    TEST_ASSERT_MESSAGE(
+        roundtrip("test/data/BUILD.load_2") == EXIT_SUCCESS,
+        "Mismatch on test/data/BUILD.load_2"
+    );
+    TEST_ASSERT_MESSAGE(
+        roundtrip("test/data/BUILD.load_3") == EXIT_SUCCESS,
+        "Mismatch on test/data/BUILD.load_3"
+    );
     TEST_ASSERT_MESSAGE(
         roundtrip("test/data/BUILD.load_aliases_1") == EXIT_SUCCESS,
         "Mismatch on test/data/BUILD.load_aliases_1"
@@ -56,11 +73,40 @@ void test_load_aliases(void) {
     );
 }
 
+void test_python(void) {
+    TEST_ASSERT_MESSAGE(
+        roundtrip("test/data/BUILD.py") == EXIT_SUCCESS,
+        "Mismatch on test/data/BUILD.py"
+    );
+
+    TEST_ASSERT_MESSAGE(
+        roundtrip("test/data/BUILD.py_native") == EXIT_SUCCESS,
+        "Mismatch on test/data/BUILD.py_native"
+    );
+}
+
+void test_shell(void) {
+    TEST_ASSERT_MESSAGE(
+        roundtrip("test/data/BUILD.shell") == EXIT_SUCCESS,
+        "Mismatch on test/data/BUILD.shell"
+    );
+}
+
+void test_windows(void) {
+    TEST_ASSERT_MESSAGE(
+        roundtrip("test/data/BUILD.windows_dll") == EXIT_SUCCESS,
+        "Mismatch on test/data/BUILD.windows_dll"
+    );
+}
+
 int main(void) {
     UNITY_BEGIN();
+    RUN_TEST(test_android);
     RUN_TEST(test_cpp);
-    RUN_TEST(test_filegroup);
-    RUN_TEST(test_gen);
-    RUN_TEST(test_load_aliases);
+    RUN_TEST(test_general_rules);
+    RUN_TEST(test_load);
+    RUN_TEST(test_python);
+    RUN_TEST(test_shell);
+    RUN_TEST(test_windows);
     return UNITY_END();
 }
