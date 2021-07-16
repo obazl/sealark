86238252e43e8dd4100fd22473993134acf4e510
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2021-07-16 06:56:57 -0500

    rename node_dtor -> ast_node_free

--- src/lib/starlark/parse.c
+++ src/lib/starlark/parse.c
@@ -59,7 +59,7 @@ EXPORT void parser_free(parse_state_s *parser)
 {
     // log_debug("parser_free %s", parser->lexer->fname);
     lexer_free(parser->lexer);
-    node_dtor(parser->root);
+    ast_node_free(parser->root);
 }
 
 EXPORT UT_array *starlark_lex_string(const char *buffer)
