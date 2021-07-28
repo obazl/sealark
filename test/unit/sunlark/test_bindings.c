4c784b7b7f5ef3202d0cea90fe02afb4d7a299e5
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2021-07-28 08:49:25 -0500

    add bindings test, with iterator

--- test/unit/sunlark/test_bindings.c
+++ test/unit/sunlark/test_bindings.c
@@ -8,13 +8,13 @@
 #include "sealark.h"
 #include "sunlark.h"
 
-#include "bindings.h"
+#include "test_bindings.h"
 
 UT_string *buf;
 UT_string *test_s;
 UT_array  *result;
 
-char *build_file = "test/unit/sunlark/BUILD.test1";
+char *build_file = "test/unit/sunlark/BUILD.bindings";
 
 s7_scheme *s7;
 
@@ -23,13 +23,9 @@ s7_scheme *s7;
 s7_pointer ast;
 struct node_s *root;
 
-s7_pointer is_eq_s7;
-s7_pointer is_equal_s7;
-
 void setUp(void) {
     s7 = sunlark_init();
-    is_eq_s7 = s7_name_to_value(s7, "eq?");
-    is_equal_s7 = s7_name_to_value(s7, "equal?");
+    init_s7_syms(s7);
     ast = sunlark_parse_build_file(s7,
                                    s7_list(s7, 1,
                                            s7_make_string(s7, build_file)));
@@ -40,66 +36,149 @@ void tearDown(void) {
     s7_quit(s7);
 }
 
-void test_bindings(void) {
-    s7_pointer path1 = s7_eval_c_string(s7, "'(:targets 1 :bindings)");
-    s7_pointer bindings1 = s7_apply_function(s7, ast, path1);
-    /* check type, tid */
-    TEST_ASSERT( !s7_is_c_object(bindings1) );
-    TEST_ASSERT( s7_is_list(s7, bindings1) );
-    TEST_ASSERT( s7_list_length(s7, bindings1) == 3 );
+void test_forall_targets_forall_bindings(void) {
+    s7_pointer path = s7_eval_c_string(s7,
+                       "'(:targets :bindings)");
+    s7_pointer targets = s7_apply_function(s7, ast, path);
 
-    s7_pointer path2 = s7_eval_c_string(s7, "'(:targets 1 :bindings)");
+    /* check type, tid */
+    TEST_ASSERT( !s7_is_c_object(targets) );
+    TEST_ASSERT( s7_is_list(s7, targets) );
+    log_debug("len 1: %d", s7_list_length(s7, targets));
+    TEST_ASSERT( s7_list_length(s7, targets) == 18 );
+
+    /* same with concise op */
+    s7_pointer path2 = s7_eval_c_string(s7,
+                       "'(:>> :@@)");
+    s7_pointer targets2 = s7_apply_function(s7, ast, path2);
+
+    /* check type, tid */
+    TEST_ASSERT( !s7_is_c_object(targets2) );
+    TEST_ASSERT( s7_is_list(s7, targets2) );
+    log_debug("len 2: %d", s7_list_length(s7, targets2));
+    TEST_ASSERT( s7_list_length(s7, targets2) == 18 );
+
+    /* we can access a single target using a path expression, as in
+       test_target below. but here, since targets is a Scheme list,
+       we must use Scheme list operations: */
+    s7_pointer target;
+    while ( !s7_is_null(s7, targets) ) {
+        target = s7_car(targets);
+        TEST_ASSERT( s7_is_c_object(target) );
+        TEST_ASSERT( s7_c_object_type(target) == AST_NODE_T );
+        /* (sunlark-node? target) => #t */
+        s7_pointer is_node = s7_apply_function(s7, is_sunlark_node_s7,
+                                               s7_cons(s7, target,
+                                                       s7_nil(s7)));
+        TEST_ASSERT( is_node == s7_t(s7) );
+        /* (list? target) => #f */
+        s7_pointer is_list = s7_apply_function(s7, is_list_s7,
+                                               s7_cons(s7, target,
+                                                       s7_nil(s7)));
+        TEST_ASSERT( is_list == s7_f(s7) );
+
+        targets = s7_cdr(targets);
+    }
+}
+
+void test_map_over_bindings(void) {
+    s7_pointer path = s7_eval_c_string(s7,
+                       "'(:>> :@@)");
+    s7_pointer targets = s7_apply_function(s7, ast, path);
+
+    /* check type, tid */
+    TEST_ASSERT( !s7_is_c_object(targets) );
+    TEST_ASSERT( s7_is_list(s7, targets) );
+    TEST_ASSERT( s7_list_length(s7, targets) == 6 );
+
+    /* we can access a single target using a path expression, as in
+       test_target below. but here, since targets is a Scheme list,
+       we must use Scheme list operations: */
+    s7_pointer target;
+    while ( !s7_is_null(s7, targets) ) {
+        target = s7_car(targets);
+        TEST_ASSERT( s7_is_c_object(target) );
+        TEST_ASSERT( s7_c_object_type(target) == AST_NODE_T );
+        /* (sunlark-node? target) => #t */
+        s7_pointer is_node = s7_apply_function(s7, is_sunlark_node_s7,
+                                               s7_cons(s7, target,
+                                                       s7_nil(s7)));
+        TEST_ASSERT( is_node == s7_t(s7) );
+        /* (list? target) => #f */
+        s7_pointer is_list = s7_apply_function(s7, is_list_s7,
+                                               s7_cons(s7, target,
+                                                       s7_nil(s7)));
+        TEST_ASSERT( is_list == s7_f(s7) );
+
+        targets = s7_cdr(targets);
+    }
+}
+
+void test_binding_list(void) {
+    s7_pointer path1 = s7_eval_c_string(s7, "'(:target 1 :bindings)");
+    s7_pointer bindings1 = s7_apply_function(s7, ast, path1);
+
+    /* (bindings1 :arg-list?) =? #t */
+    s7_pointer pred = s7_apply_function(s7, bindings1,
+                             s7_eval_c_string(s7, "'(:arg-list?)"));
+    TEST_ASSERT( pred == s7_t(s7) );
+    /* (bindings1 :bindings?) =? #t */
+    pred = s7_f(s7);
+    pred = s7_apply_function(s7, bindings1,
+                             s7_eval_c_string(s7, "'(:bindings?)"));
+    TEST_ASSERT( pred == s7_t(s7) );
+
+    s7_pointer ct = s7_apply_function(s7,
+                                      s7_eval_c_string(s7, "length"),
+                                      s7_list(s7, 1, bindings1));
+    TEST_ASSERT( s7_integer(ct) == 3 ); // 3 elts, 5 including commas
+
+    /* check type, tid */
+    TEST_ASSERT( s7_is_c_object(bindings1) );
+    TEST_ASSERT( !s7_is_list(s7, bindings1) );
+
+    /* same again, for equality testing */
+    s7_pointer path2 = s7_eval_c_string(s7, "'(:target 1 :bindings)");
     s7_pointer bindings2 = s7_apply_function(s7, ast, path2);
-    TEST_ASSERT( !s7_is_c_object(bindings2) );
-    TEST_ASSERT( s7_is_list(s7, bindings2) );
-    TEST_ASSERT( s7_list_length(s7, bindings2) == 3 );
+    TEST_ASSERT( s7_is_c_object(bindings2) );
+    TEST_ASSERT( !s7_is_list(s7, bindings2) );
 
     /* from same path we get equal? but not eq? */
-    s7_pointer are_eq = s7_apply_function(s7, is_eq_s7,
-                                          s7_list(s7, 2,
-                                                  bindings1, bindings2));
-    TEST_ASSERT( are_eq == s7_f(s7) );
-    s7_pointer are_equal = s7_apply_function(s7, is_equal_s7,
-                                          s7_list(s7, 2,
-                                                  bindings1, bindings2));
-    TEST_ASSERT( are_equal == s7_t(s7) );
+    s7_pointer equality = s7_apply_function(s7, s7_eval_c_string(s7, "eq?"),
+                                            s7_list(s7, 2, bindings1, bindings2));
+    TEST_ASSERT( equality == s7_f(s7) );
+    equality = s7_f(s7);
+    equality = s7_apply_function(s7, s7_eval_c_string(s7, "equal?"),
+                                 s7_list(s7, 2, bindings1, bindings2));
+    TEST_ASSERT( equality == s7_t(s7) );
 
-    /* underlying c-objects: different arrays with same vals */
-    /* equality over list elements (nodes) */
-    s7_pointer binding1, binding2;
-    struct node_s *binding1_o;
-    struct node_s *binding2_o;
-    while ( !s7_is_null(s7, bindings1) ) {
-        binding1 = s7_car(bindings1);
-        binding2 = s7_car(bindings2);
+    /* binding (arg) lists are not Scheme lists nor vectors, but they
+       are s7-iterable; (for-each (lambda (binding) ...) bindings1) */
+    /* s7_pointer binding1, binding2; */
+    /* struct node_s *binding1_o; */
+    /* struct node_s *binding2_o; */
+    s7_pointer iter1 = s7_make_iterator(s7, bindings1);
+    s7_pointer iter2 = s7_make_iterator(s7, bindings2);
+    s7_pointer binding1 = s7_iterate(s7, iter1);
+    s7_pointer binding2 = s7_iterate(s7, iter2);
+    while ( ! s7_iterator_is_at_end(s7, iter1) ) {
         TEST_ASSERT( s7_is_c_object(binding1) );
-        TEST_ASSERT( s7_is_c_object(binding2) );
-        TEST_ASSERT( s7_c_object_type(binding1) == AST_NODE_T );
-        TEST_ASSERT( s7_c_object_type(binding2) == AST_NODE_T );
+        TEST_ASSERT( !s7_is_list(s7, binding1) );
+        pred = s7_apply_function(s7,
+                                 binding1,
+                                 s7_eval_c_string(s7, "'(:binding?)"));
+        TEST_ASSERT( pred == s7_t(s7) );
 
-        are_eq = s7_apply_function(s7, is_eq_s7,
-                                   s7_list(s7, 2,
-                                           binding1, binding2));
-        TEST_ASSERT( are_eq == s7_f(s7) ); /* not eq? */
+        equality = s7_apply_function(s7, s7_eval_c_string(s7, "eq?"),
+                                     s7_list(s7, 2, binding1, binding2));
+        TEST_ASSERT( equality == s7_f(s7) ); /* not eq? */
+        equality = s7_f(s7);
+        equality = s7_apply_function(s7, s7_eval_c_string(s7, "equal?"),
+                                 s7_list(s7, 2, binding1, binding2));
+        TEST_ASSERT( equality == s7_t(s7) ); /* but equal? */
 
-        are_equal = s7_apply_function(s7, is_equal_s7,
-                                      s7_list(s7, 2,
-                                              binding1, binding2));
-        TEST_ASSERT( are_equal == s7_t(s7) ); /* are equal? */
-
-        binding1_o = s7_c_object_value(binding1);
-        binding2_o = s7_c_object_value(binding2);
-        TEST_ASSERT( binding1_o != binding2_o );
-
-        TEST_ASSERT( binding1_o->tid == binding2_o->tid );
-        TEST_ASSERT( &(binding1_o->tid) != &(binding2_o->tid) );
-
-        TEST_ASSERT( binding1_o->subnodes == binding2_o->subnodes );
-        TEST_ASSERT( utarray_len(binding1_o->subnodes)
-                     == utarray_len(binding2_o->subnodes) );
-
-        bindings1 = s7_cdr(bindings1);
-        bindings2 = s7_cdr(bindings2);
+        binding1 = s7_iterate(s7, iter1);
+        binding2 = s7_iterate(s7, iter2);
     }
 }
 
@@ -284,13 +363,29 @@ void test_binding_value_vector(void) {
     TEST_ASSERT( bvalue_node->tid == TK_List_Expr );
 }
 
+void test_binding_predicate(void) {
+    s7_pointer path = s7_eval_c_string(s7,
+                       "'(:> \"hello-lib\" :@ srcs 0)");
+    s7_pointer binding = s7_apply_function(s7, ast, path);
+    log_debug("binding: %s", s7_object_to_c_string(s7, binding));
+    /* check type, tid */
+    TEST_ASSERT( s7_is_c_object(binding) );
+    log_debug("binding tid: %s", s7_object_to_c_string(s7, binding));
+    TEST_ASSERT( sunlark_node_tid(s7, binding) == TK_Binding );
+
+    struct node_s *binding_node = s7_c_object_value(binding);
+    TEST_ASSERT( binding_node->tid == TK_List_Expr );
+}
+
 int main(void) {
     UNITY_BEGIN();
-    RUN_TEST(test_bindings);
-    RUN_TEST(test_binding);
-    RUN_TEST(test_binding_key);
-    RUN_TEST(test_binding_value_string_plain_dq);
-    RUN_TEST(test_binding_value_string_plain_sq);
+    /* RUN_TEST(test_forall_targets_forall_bindings); */
+    RUN_TEST(test_binding_list);
+    /* RUN_TEST(test_binding); */
+    /* RUN_TEST(test_binding_key); */
+    /* RUN_TEST(test_binding_value_string_plain_dq); */
+    /* RUN_TEST(test_binding_value_string_plain_sq); */
     /* RUN_TEST(test_binding_value_vector); */
+    /* RUN_TEST(test_binding_predicate); */
     return UNITY_END();
 }
