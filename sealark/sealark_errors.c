60fb45bbb3b4b9e00bc6f7b0ab805acbbf7f35f1
Gregg Reynolds <601396+mobileink@users.noreply.github.com>
2021-08-02 07:14:36 -0500

    add sealark_errors.c

---
+++ sealark/sealark_errors.c
@@ -0,0 +1,10 @@
+#include "sealark_errors.h"
+
+#if EXPORT_INTERFACE
+#define ESUNLARK_INVALID_ARG -1
+#define ESUNLARK_ARG_TYPE_ERR -2
+#define ESUNLARK_LOCN_ARG_ERR -3
+#define EINDEX_TOO_BIG -4
+#endif
+
+int e;
